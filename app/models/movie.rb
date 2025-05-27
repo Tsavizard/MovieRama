acf19c01a348073aa6b007d1fa2168c00c36987a
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-05-27 22:16:05 +0300

    feat: add Movie validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ApplicationRecord
   belongs_to :user
-  has_many :votes
+  has_many :votes, class_name: "User::Vote"
+
+  validates_associated :votes
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: :true, length: { maximum: 200 }
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid" do
+    movie = Movie.new({ title: "The Shawshank redemption",  description: "A banker is framed for the murder of his wife", user: users(:one) })
+    assert movie.valid?
+  end
+  test "must have a title" do
+    movie = Movie.new({ title: "",  description: "A banker is framed for the murder of his wife", user: users(:one) })
+    assert movie.invalid?
+  end
+
+  test "title must not exceed 50 characters length" do
+    excessive_string = (0...50).map { ("a".."z").to_a[rand(26)] }.join
+    movie = Movie.new({ title: "The Shawshank redemption " + excessive_string,  description: "A banker is framed for the murder of his wife", user: users(:one) })
+    assert movie.invalid?
+  end
+
+  test "must have a description" do
+    movie = Movie.new({ title: "The Shawshank redemption", description: "", user: users(:one) })
+    assert movie.invalid?
+
+    movie2 = Movie.new({ title: "The Shawshank redemption", description: nil, user: users(:one) })
+    assert movie2.invalid?
+  end
+
+  test "description must not exceed 200 characters length" do
+    excessive_string = (0...200).map { ("a".."z").to_a[rand(26)] }.join
+    movie = Movie.new({ title: "The Shawshank redemption",  description: "A banker is framed for the murder of his wife " + excessive_string, user: users(:one) })
+    assert movie.invalid?
+  end
 end
