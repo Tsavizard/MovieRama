528f72bcf385568d22b4f3819b91c31b2a0dd7ba
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-05-29 20:48:41 +0300

    feat: add likes, dislikes scopes to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
   has_many :votes
+  has_many :likes, -> { where(vote_type: :like) }, class_name: "Vote"
+  has_many :dislikes, -> { where(vote_type: :dislike) }, class_name: "Vote"
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   validates :email_address, format: URI::MailTo::EMAIL_REGEXP
@@ -13,5 +15,13 @@ class User < ApplicationRecord
 
   def dislike(movie)
     votes.create(movie: movie, vote_type: "dislike")
+  end
+
+  def likes?(movie)
+    likes.where(movie: movie).limit(1).present?
+  end
+
+  def dislikes?(movie)
+    dislikes.where(movie: movie).limit(1).present?
   end
 end
