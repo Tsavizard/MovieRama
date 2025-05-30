8501ea0d855e1f712ecfaf5c0f036d213128fee3
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-05-31 01:00:33 +0300

    fix: signup form path, session model and controller tests

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,40 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new to show login page" do
+    get login_url
+    assert_response :success
+  end
+
+  test "should post create to login a user" do
+    assert_difference -> { Session.count } do
+      post session_url, params: { email_address: users(:one).email_address, password: "password" }
+    end
+  end
+
+  test "should redirect to home after login" do
+    post session_url, params: { email_address: users(:one).email_address, password: "password" }
+    assert_redirected_to root_url
+  end
+
+  test "should not login with invalid credentials" do
+    assert_no_difference -> { Session.count } do
+      post session_url, params: { email_address: users(:one).email_address, password: "wrongpassword" }
+    end
+    assert_redirected_to new_session_path
+    assert_equal "Invalid credentials.", flash[:alert]
+  end
+
+  test "should get destroy to logout a user" do
+    post session_url, params: { email_address: users(:one).email_address, password: "password" }
+    assert_difference -> { Session.count }, -1 do
+      delete session_url
+    end
+  end
+
+  test "should redirect to login page after logout" do
+    post session_url, params: { email_address: users(:one).email_address, password: "password" }
+    delete session_url
+    assert_redirected_to new_session_path
+  end
+end
--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SessionTest < ActiveSupport::TestCase
   test "is valid with a user" do
-    user = User.create!(email_address: "alex@example.com", password: "123456", password_confirmation: "123456")
+    user = User.create!(email_address: "alex@example.com", password: "12345678", password_confirmation: "12345678", username: "test-user")
     session = Session.new(user: user, ip_address: "192.168.1.1", user_agent: "Mozilla/5.0")
 
     assert session.valid?, "Session should be valid when user is present"
@@ -16,7 +16,7 @@ class SessionTest < ActiveSupport::TestCase
   end
 
   test "can have nil ip_address and user_agent" do
-    user = User.create!(email_address: "alex@example.com", password: "123456", password_confirmation: "123456")
+    user = User.create!(email_address: "alex@example.com", password: "12345678", password_confirmation: "12345678", username: "test-user")
     session = Session.new(user: user)
 
     assert session.valid?, "Session should be valid even if ip_address and user_agent are nil"
