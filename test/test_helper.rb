1c8d06b3eab06fd8fde5ac74970f7bf41e06f97e
alexis stavrianos <stavrianos.alexios@gmail.com>
2025-06-01 14:09:04 +0300

    add SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,13 @@
+require "simplecov"
+
+SimpleCov.start "rails" do
+  use_merging true
+  merge_timeout 3600
+  enable_coverage :branch
+  add_filter "/test/"
+  add_filter "/config/"
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
@@ -7,6 +17,13 @@ module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
+    parallelize_setup do |worker|
+      SimpleCov.command_name "#{SimpleCov.command_name}[#{worker}]"
+    end
+
+    parallelize_teardown do |worker|
+      SimpleCov.result
+    end
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
@@ -14,8 +31,6 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
-
-
 
 
 class ActionDispatch::IntegrationTest
